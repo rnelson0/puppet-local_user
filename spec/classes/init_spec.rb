4bd470a39440fab1cfb6601275246f9c30c0634a
Rob Nelson <rnelson0@gmail.com>
2014-12-29 14:24:33 +0000

    Create successful spec tests

--- spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'local_user' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('local_user') }
-  end
-end
---
+++ spec/defines/init_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+describe 'local_user' do
+  let (:title) { 'rnelson0' }
+  let (:params) do
+  {
+    :state    => 'present',
+    :comment  => 'Rob Nelson',
+    :groups   => ['group1', 'group2'],
+    :password => 'encryptedstring',
+  }
+  end
+
+  context 'using minimum params' do
+    it { is_expected.to create_user('rnelson0').with({
+      :comment          => 'Rob Nelson',
+      :shell            => '/bin/bash',
+      :home             => '/home/rnelson0',
+      :groups           => ['group1', 'group2'],
+      :password_max_age => 90,
+    }) }
+  end
+
+  context 'using full params' do
+    let (:params) do
+    {
+      :state            => 'present',
+      :comment          => 'Rob Nelson',
+      :shell            => '/bin/zsh',
+      :home             => '/nfshome/rnelson0',
+      :groups           => ['group1', 'group2'],
+      :password         => 'encryptedstring',
+      :password_max_age => 120,
+    }
+    end
+
+    it { is_expected.to create_user('rnelson0').with({
+      :comment          => 'Rob Nelson',
+      :shell            => '/bin/zsh',
+      :home             => '/nfshome/rnelson0',
+      :groups           => ['group1', 'group2'],
+      :password_max_age => 120,
+    }) }
+  end
+end
