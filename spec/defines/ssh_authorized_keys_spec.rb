52dc92b7598bea3a7563a6ad7c099d64e82cc24f
Rob Nelson <rnelson0@gmail.com>
2015-03-26 03:14:12 +0000

    (#5) Feature request to add ssh authorized keys   Create new type local_user::ssh_authorized_keys   Add parameter ssh_authorized_keys (array) to local_user type   Create/update rspec tests to address the new functionality

---
+++ spec/defines/ssh_authorized_keys_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+describe 'local_user::ssh_authorized_keys' do
+  let (:title) { 'ssh-rsa AAAA...zwE1 rsa-key-20141029' }
+  let (:params) do
+  {
+    :user => 'rnelson0',
+  }
+  end
+
+  context 'default' do
+    it { is_expected.to create_ssh_authorized_key('rnelson0_ssh-rsa_rsa-key-20141029').with({
+      :user  => 'rnelson0',
+      :type  => 'ssh-rsa',
+      :key   => 'AAAA...zwE1',
+      :name  => 'rsa-key-20141029',
+    }) }
+  end
+end
