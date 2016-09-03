2a89cc6ee9f6bed9a86ff9816c4d8151f7ea8847
Chris Jowett <cryptk@gmail.com>
2016-09-03 03:09:29 +0000

    Update init_spec.rb to account for new user detection

--- spec/defines/init_spec.rb
+++ spec/defines/init_spec.rb
@@ -5,13 +5,14 @@ describe 'local_user', :type => :define do
     {
       :state    => 'present',
       :groups   => ['group1', 'group2'],
-      :password => 'encryptedstring',
+#      :password => 'encryptedstring',
     }
   end
 
   let (:facts) do
     {
-      :osfamily => 'Debian',
+      :osfamily    => 'Debian',
+      :linux_users => ['nobody'],
     }
   end
 
@@ -22,9 +23,9 @@ describe 'local_user', :type => :define do
       :home             => '/home/rnelson0',
       :groups           => ['group1', 'group2'],
       :password_max_age => 90,
+      :password         => '*',
     }) }
     it { is_expected.not_to create_group('rnelson0') }
-    it { is_expected.to create_exec("set rnelson0's password") }
   end
 
   context 'managing all groups' do
@@ -122,6 +123,7 @@ context 'manage_groups with invalid input' do
       :home             => '/nfshome/rnelson0',
       :groups           => ['group1', 'group2'],
       :password_max_age => 120,
+      :password         => 'encryptedstring',
       :uid              => 101,
       :system           => true
     }) }
